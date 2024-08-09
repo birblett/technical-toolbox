353f1512713b1379631bf17c9bb201eeccf57940
birblett <bwee.sylv@gmail.com>
2024-08-08 23:58:16 -0700

    update carpet accessor mixin version

--- src/main/java/com/birblett/compat/mixin/Carpet_CarpetClientAccessor.java
+++ src/main/java/com/birblett/compat/mixin/Carpet_CarpetClientAccessor.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
-@MixinPlugin.Requires("carpet")
+@MixinPlugin.Requires({"carpet", "1.4.147+v240613"})
 @Pseudo
 @Mixin(targets = "carpet.network.CarpetClient")
 public interface Carpet_CarpetClientAccessor {
